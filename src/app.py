8da86dcc650fe4d7be4848bd6e60912461fa4d00
横山光希 <koki@yokoyamamitsukinoMacBook-Air-2.local>
2025-06-13 23:34:58 +0900

    set page config

--- src/app.py
+++ src/app.py
@@ -2,9 +2,12 @@ import streamlit as st
 from PIL import Image
 
 from libs.config import load_config
-from libs.utils import load_css
+from libs.utils import load_css, set_page_config
 
 config = load_config()
+
+# set page config
+set_page_config()
 
 # set style
 load_css()
